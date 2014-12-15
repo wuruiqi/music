2eee612997d61784465562716079fc742e533af5
woshiwuruiqi <woshiwuruiqi@gmail.com>
2014-12-15 21:01:51 +0800

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server" Width="178px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="But7" runat="server" Text="7" Width="29px" OnClick="But7_Click" style="height: 21px" />
+&nbsp;
+        <asp:Button ID="But8" runat="server" Text="8" Width="29px" OnClick="But8_Click" />
+&nbsp;
+        <asp:Button ID="But9" runat="server" Text="9" Width="29px" OnClick="But9_Click" />
+&nbsp;
+        <asp:Button ID="ButDivide" runat="server" Text="/" Width="29px" OnClick="ButDivide_Click" />
+        <br />
+        <br />
+        <asp:Button ID="But4" runat="server" Text="4" Width="29px" OnClick="But4_Click" />
+&nbsp;
+        <asp:Button ID="But5" runat="server" Text="5" Width="29px" OnClick="But5_Click" />
+&nbsp;
+        <asp:Button ID="But6" runat="server" Text="6" Width="29px" OnClick="But6_Click" />
+&nbsp;
+        <asp:Button ID="ButMulti" runat="server" Text="*" Width="29px" OnClick="ButMulti_Click" />
+        <br />
+        <br />
+        <asp:Button ID="But1" runat="server" Text="1" Width="29px" OnClick="But1_Click" />
+&nbsp;
+        <asp:Button ID="But2" runat="server" Text="2" Width="29px" OnClick="But2_Click" />
+&nbsp;
+        <asp:Button ID="But3" runat="server" Text="3" Width="29px" OnClick="But3_Click" />
+&nbsp;
+        <asp:Button ID="ButMinus" runat="server" Text="-" Width="29px" OnClick="ButMinus_Click" />
+        <br />
+        <br />
+        <asp:Button ID="But0" runat="server" Text="0" Width="29px" OnClick="But0_Click" />
+&nbsp;<asp:Button ID="Butclear" runat="server" Text="清空" Width="47px" OnClick="Butclear_Click" style="height: 21px" />
+        <asp:Button ID="Butsum" runat="server" Text="计算" Width="40px" OnClick="Butsum_Click" />
+&nbsp;
+        <asp:Button ID="ButPlus" runat="server" Text="+" Width="29px" OnClick="ButPlus_Click" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+  
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    public void result()  //求和算法//
+    { 
+        string symbol = (string)Session["symbol"];
+       switch(symbol)
+       {case "+":
+           TextBox1.Text = Convert.ToString(Convert.ToInt32(Session["text1"]) + Convert.ToInt32(TextBox1.Text));
+        Session["symbol"] = "";
+        Session["text1"] = TextBox1.Text;
+
+        break;
+       case "-":
+        TextBox1.Text = Convert.ToString(Convert.ToInt32(Session["text1"]) - Convert.ToInt32(TextBox1.Text));
+        Session["symbol"] = "";
+        Session["text1"] = TextBox1.Text;
+        break;
+       case "*":
+        TextBox1.Text = Convert.ToString(Convert.ToInt32(Session["text1"]) * Convert.ToInt32(TextBox1.Text));
+        Session["symbol"] = "";
+        Session["text1"] = TextBox1.Text;
+        break;
+       case "/":
+               if (Session["text1"]=="0")
+                   TextBox1.Text = "除数不能为零！";
+        TextBox1.Text = Convert.ToString(Convert.ToInt32(Session["text1"]) / Convert.ToInt32(TextBox1.Text));
+        Session["symbol"] = "";
+        Session["text1"] = TextBox1.Text;
+        break;
+    }
+    }
+    protected void Butsum_Click(object sender, EventArgs e) //求和//
+    {
+        result();        
+    }
+   
+    protected void ButPlus_Click(object sender, EventArgs e)//加法//
+    {
+        
+        if (Session["symbol"] != "+" & Session["symbol"] != "-" & Session["symbol"] != "*" & Session["symbol"] != "/")
+               {
+                Session["symbol"] = "+";            
+            Session["text1"] = TextBox1.Text;
+            TextBox1.Text = "";
+            
+        }
+        else
+        {
+            result();
+            Session["symbol"] = "+";
+            TextBox1.Text = "";
+
+        }
+    }
+    protected void ButMinus_Click(object sender, EventArgs e)  //减法//
+    {
+        if (Session["symbol"] != "+" & Session["symbol"] != "-" & Session["symbol"] != "*" & Session["symbol"] != "/")
+        {
+            Session["symbol"] = "-";
+            Session["text1"] = TextBox1.Text;
+            TextBox1.Text = "";
+
+        }
+        else
+        {
+            result();
+            Session["symbol"] = "-";
+            TextBox1.Text = "";
+
+        }
+    }
+    protected void ButMulti_Click(object sender, EventArgs e)//乘法//
+    {
+        if (Session["symbol"] != "+" & Session["symbol"] != "-" & Session["symbol"] != "*" & Session["symbol"] != "/")
+        {
+            Session["symbol"] = "*";
+            Session["text1"] = TextBox1.Text;
+            TextBox1.Text = "";
+
+        }
+        else
+        {
+            result();
+            Session["symbol"] = "*";
+            TextBox1.Text = "";
+        }
+    }
+    protected void ButDivide_Click(object sender, EventArgs e)//除法//
+    {
+        if (Session["symbol"] != "+" & Session["symbol"] != "-" & Session["symbol"] != "*" & Session["symbol"] != "/")
+        {
+            Session["symbol"] = "/";
+            Session["text1"] = TextBox1.Text;
+            TextBox1.Text = "";
+
+        }
+        else
+        {
+            result();
+            Session["symbol"] = "/";
+            TextBox1.Text = "";
+
+        }
+    }
+   
+    protected void Butclear_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text = "";
+        Session["text1"] = "";
+        Session["text2"] = "";
+        Session["symbol"] = "";
+       
+    }
+    protected void But0_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "0";
+    }
+    protected void But1_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "1";
+
+    }
+
+    protected void But2_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "2";
+    }
+    protected void But3_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "3";
+    }
+    protected void But4_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "4";
+    }
+    protected void But5_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "5";
+    }
+    protected void But6_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "6";
+    }
+    protected void But7_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "7";
+    }
+    protected void But8_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "8";
+    }
+    protected void But9_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text += "9";
+    }
+ 
+}
